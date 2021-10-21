26e288f37e49c8f5f94ecce25cf98a2b6821b586
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2021-10-20 23:02:20 -0300

    Multiple categories

--- js/main.js
+++ js/main.js
@@ -5,27 +5,46 @@ const filterString = (str) => str
 	.normalize('NFD')
 	.replace(/[^\x20-\x7f]/g, '');
 
+const softCompare = (a, b) => filterString(a) === filterString(b);
+
+const categoriesToString = (categories) => {
+	return categories
+		.map((category) => category.name)
+		.join('; ');
+};
+
 const getDB = async () => {
 	const text = await $.ajax({
 		url: './list.txt',
 		method: 'GET',
 	});
 	const lines = text.trim().split(/\s*\n\s*/);
-	let category = null;
+	let currentCategories = [];
 	const categories = [];
+	const addCategory = (name) => {
+		let category = categories.find((item) => softCompare(item.name, name));
+		if (category != null) return category;
+		const id = categories.length + 1;
+		category = { id, name };
+		categories.push(category);
+		return category;
+	};
 	const items = [];
 	for (let line of lines) {
 		if (line.startsWith('*')) {
-			const name = line.substr(1).trim();
-			category = { id: categories.length + 1, name };
-			categories.push(category);
+			const names = line.substr(1).trim().split(/\s*\+\s*/);
+			currentCategories = [];
+			for (let name of names) {
+				category = addCategory(name);
+				currentCategories.push(category);
+			}
 			continue;
 		}
 		const name = line.replace(/^\!\s*/, '');
 		const search = filterString(name);
 		items.push({
 			name,
-			category,
+			categories: currentCategories.slice(),
 			inventory: !line.startsWith('!'),
 			search,
 		});
@@ -42,20 +61,23 @@ const appendItem = (item, parent) => {
 		<div class="info"></div>
 	`);
 	div.find('.name').text(item.name);
-	div.find('.info').eq(0).text(item.category.name);
+	div.find('.info').eq(0).text(categoriesToString(item.categories));
 	div.find('.info').eq(1).text(`Em estoque: ${item.inventory ? 'sim' : 'não'}`);
 	if (!item.inventory) {
 		div.addClass('empty-inventory');
 	}
 };
 
-const updateList = ({ items, categories }) => {
+const updateList = ({ items }) => {
 	const list = $('#list');
 	list.html('');
 	const catId = parseInt($('select').val());
 	const search = filterString($('input').val());
 	const result = items.filter((item) => {
-		if (catId !== 0 && item.category.id !== catId) return false;
+		if (
+			catId !== 0 &&
+			!item.categories.find(({ id }) => id === catId)
+		) return false;
 		if (!item.search.includes(search)) return false;
 		return true;
 	});
